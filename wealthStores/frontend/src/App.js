d8a582ddd6492ae7f544db8fd76f2cbb8f777879
Bowealth <adebowalemabadeje@gmail.com>
2023-10-17 15:33:52 +0100

    Wealth Stores

---
+++ wealthStores/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">WealthStores</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
